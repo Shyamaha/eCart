7cc367e2c607a6ed6fbb3e10e6810c51c427766a
Shyam <ssundar@adobe.com>
2022-07-04 17:07:20 +0530

    code updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <div className='hero'>
             <div className="card bg-dark text-white border-0">
-            <img src="eCart/assets/bg.jpg" className="card-img" alt="Background" height="900px"/>
+            <img src="/eCart/assets/bg.jpg" className="card-img" alt="Background" height="900px"/>
             <div className="card-img-overlay d-flex flex-column justify-content-center">
                 <div className="container">
                 <h5 className="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
